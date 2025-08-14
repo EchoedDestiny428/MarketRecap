8b0b711a4cf44ca22017b70ede48e40d8f36e3bf
EchoedDestiny428 <zacharyng428@gmail.com>
2025-08-14 18:32:03 +0800

    import modules

--- MarketRecap.py
+++ MarketRecap.py
@@ -1,13 +1,2 @@
 import requests
-from bs5 import BeautifulSoup
-
-def addurl(url):
-    with open('SiteLists.txt', 'a') as file:
-        file.write(url + '\n')
-
-def getSiteNumber(siteNumber):
-    with open('siteList.txt', 'r') as file:
-        return file.readline(siteNumber).strip('\n')
-    
-
-print(getSiteNumber(0))
+from bs4 import BeautifulSoup
