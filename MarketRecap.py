d8d0d84cdf47ba3fbdb78bc354bb18df5eb78a0e
EchoedDestiny428 <zacharyng428@gmail.com>
2025-08-15 13:50:52 +0800

    Webscraper class

--- MarketRecap.py
+++ MarketRecap.py
@@ -30,8 +30,44 @@ class SiteListManager:
     def clearAllSites(self):
         with open(self.filename, "w") as f:
             f.write("")
+
+class WebScraper:
+    def __init__(self, url):
+        self.url = url
+        self.soup = None
+
+    def open_website(self):
+        try:
+            response = requests.get(self.url)
+            response.raise_for_status()
+            self.soup = BeautifulSoup(response.text, "html.parser")
+            return True
+        except requests.RequestException:
+            self.soup = None
+            return False
+
+    def get_title(self):
+        if self.soup:
+            return self.soup.title.string if self.soup.title else None
+        return None
+
+    def get_all_links(self):
+        if self.soup:
+            return [a.get('href') for a in self.soup.find_all('a', href=True)]
+        return []
+
+    def get_text(self):
+        if self.soup:
+            return self.soup.get_text()
+        return ""
+
     
 SiteListManager = SiteListManager()
-SiteListManager.addSite("https://www.marketwatch.com/market-data/markets")
-SiteListManager.addSite("https://www.bloomberg.com/markets")
-print(SiteListManager.getSite(1))
+SiteListManager.addSite("https://www.marketwatch.com/markets")
+
+WebScraper = WebScraper(SiteListManager.getSite(1))
+
+if WebScraper.open_website():
+    print("Title:", WebScraper.get_title())
+    print("Links:", WebScraper.get_all_links())
+    print("Text:", WebScraper.get_text())
