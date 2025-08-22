8fd9c4542702bc0be94fd56d369852efb29ae469
EchoedDestiny428 <zacharyng428@gmail.com>
2025-08-22 21:49:03 +0800

    delete scraper

--- MarketRecap.py
+++ MarketRecap.py
@@ -40,42 +40,6 @@ class SiteListManager:
         with open(self.filename, "w") as f:
             f.write("")
 
-class WebScraper:
-    def __init__(self, url):
-        self.url = url
-        self.driver = None
-        self.soup = None
-
-    def open_website(self):
-        try:
-            self.driver = webdriver.Firefox()
-            self.driver.get(self.url)
-            html = self.driver.page_source
-            self.soup = BeautifulSoup(html, "html.parser")
-            return True
-        except Exception:
-            self.soup = None
-            return False
-
-    def get_title(self):
-        if self.soup:
-            return self.soup.title.string if self.soup.title else None
-        return None 
-
-    def get_all_links(self):
-        if self.soup:
-            return [a.get('href') for a in self.soup.find_all('a', href=True)]
-        return []
-
-    def get_text(self):
-        if self.soup:
-            return self.soup.get_text()
-        return ""
-
-    def close(self):
-        if self.driver:
-            self.driver.quit()
-
 class PriceManager:
     def __init__(self, api_key):
         self.api_key = api_key
@@ -97,11 +61,6 @@ class PriceManager:
         return response
 
 
-SiteListManager = SiteListManager()
-SiteListManager.clearAllSites()
-SiteListManager.addSite("https://www.marketwatch.com/markets")
-SiteListManager.addSite("https://google.com")
-
 polykey = '3xCm_W1ug8HuguFEtibvELIUHZz0OlCh'
 
 
@@ -109,11 +68,3 @@ PriceManager = PriceManager(polykey)
 if PriceManager.get_data():
     print("Price data retrieved successfully.")
     print(PriceManager.get_data()) 
-
-'''
-WebScraper = WebScraper(SiteListManager.getSite(1))
-if WebScraper.open_website():
-    print("Title:", WebScraper.get_title())
-    print("Text Content:", WebScraper.get_text()[:200]) 
-    time.sleep(100)
-'''
